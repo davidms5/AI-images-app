122327f69a35eef3d221682917b650aa3c5d61bb
david medina <rafaelsanchez97.dm@gmail.com>
2023-03-01 18:51:02 -0300

    componente crear posts casi completado

--- src/pages/crearPost.jsx
+++ src/pages/crearPost.jsx
@@ -15,10 +15,52 @@ const CrearPost = () =>{
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = e =>{
+
+  }
+
   return (
-    <div>
-    crear post
+    <section className='max-w-7xl mx-auto'>
+      <div>
+       <h1 className='font-extrabold text-[#222328] text-[16px]'>Crear</h1> 
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'> crea imaginativas y visualmente impresionantes imagenes generadas con DALL-E AI y compartelas con la comunidad!</p>
+
     </div>
+
+    <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+      <div className='flex flex-col gap-5'>
+
+      <FormField
+      LabeLName='tu nombre'
+      type="text"
+      name='name'
+      placeholder='john'
+      value={form.name} {//ver de donde sale form.name}
+      handleChange={handleChange}
+       />
+
+      <FormField
+      LabeLName="prompt"
+      type="text"
+      name="prompt"
+      placeholder="a plush toy robot sitting against a blue wall"
+      value={form.prompt}
+      handleChange={handleChange}
+      isSurpriseMe
+      handleSurpriseMe={handleSurpriseMe}
+      />
+
+
+    </div>
+
+    </form>
+
+    </section>
   )
 }
 
